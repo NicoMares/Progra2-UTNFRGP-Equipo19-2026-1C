4bdaba1f5c4828138518d6603d1983b05fb5e5a3
Nicolas Mares <nikomares04@gmail.com>
2026-06-02 20:25:17 -0300

    Menu Empezado

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Menu.h"
+using namespace std;
+
+int main()
+{
+ system("cls");
+
+        cout<<"funca"<<endl;
+
+
+        
+  Menu menu;
+  menu.Mostrar();
+   
+    
+        return 0;
+        }/// Cierra Programa
+
+        
