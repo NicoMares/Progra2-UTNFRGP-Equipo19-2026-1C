49c7966d12ea8468b1e89c39a5ef508eb912e358
LeandroEze2004 <mikolajewski.04@gmail.com>
2026-06-03 23:17:03 -0300

    ULTIMA MODIFICACIÓN 23:15hs 03/06/26
    Hola, Acá simplemente agregue las Funciones Cargar a sus respectivos (Entrenador, Jugador y Club)
    Tambien cambie el Racha del MenuClub al MenuTablas.

---
+++ Fecha.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include "Fecha.h"
+#include <cstdlib>
+
+Fecha::Fecha() {
+    _Dia = 0 ;
+    _Mes = 0 ;
+    _Anio = 0;
+}
+
+void Fecha::cargar() {
+    int dia,mes,anio;
+    std::cout<< "ingrese el dia" <<std::endl;
+    std::cin>>dia;
+    set_Dia(dia);
+    std::cout<< "ingrese el mes " <<std::endl;
+    std::cin>>mes;
+    set_Mes(mes);
+    std::cout<< "ingrese el año "<< std::endl;
+    std::cin>>anio;
+    set_Anio(anio);
+}
+
+void Fecha::mostrar(){
+    std::cout<<_Dia<< "/"<<_Mes << "/"<<_Anio<<std::endl;
+
+}
+
+
+int Fecha::get_Dia() {
+    return _Dia;
+
+}
+
+int Fecha::get_Mes() {
+    return _Mes;
+}
+int Fecha::get_Anio( ) {
+return _Anio;
+
+}
+
+/*VER SI SE NECESITA MAS CONDICIONES PARA CARGAR*/
+void Fecha::set_Dia( int dia){
+    if (dia > 0 && dia <=31 ){
+
+        _Dia = dia;
+    }
+    else {
+        exit (-1);
+    }
+}
+
+void Fecha::set_Mes(int mes ){
+    if (mes > 0 && mes < 13){
+
+        _Mes = mes;
+    }
+    else {
+        exit (-1);
+    }
+}
+
+void Fecha::set_Anio(int anio ){
+    _Anio = anio;
+
+}
---
+++ Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Menú.h"
+using namespace std;
+
+int main()
+{
+ system("cls");
+
+  Menu menu;
+  menu.run();
+
+        return 0;
+        }
