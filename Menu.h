6445a8bc77db35badd7b8c9f4c71e0a0c0bfbb44
LeandroEze2004 <mikolajewski.04@gmail.com>
2026-06-03 23:37:06 -0300

    Ultima modificación
    Hola, Agregue las FUNCIONES "cargar" a los subgrupos Jugadores-Clubes-Entrenador
    Tambien Cambie la Racha que estaba en el menuClub y lo pase al menuTablas
    Modifique el nombre del archivo y le saque la tilde a "Menú"
    tambien modifique los #Include

---
+++ Menu.h
@@ -0,0 +1,22 @@
+#ifndef MANU_H_INCLUDED
+#define MANU_H_INCLUDED
+
+class Menu
+{
+public:
+
+Menu();
+void Mostrar();
+void run();
+
+private:
+void menuEquipos();
+void menuJugadores();
+void menuEntrenadores();
+void menuPartidos();
+void menuAcciones();
+void menuTabla();
+
+};
+
+#endif // MANU_H_INCLUDED
